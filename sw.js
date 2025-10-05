1a225a20a5c9adce97ecdd87580652dc106aee30
birranicolas-pixel <birranicolas@gmail.com>
2025-10-05 09:46:45 +0200

    bug service worker

--- sw.js
+++ sw.js
@@ -1,12 +1,41 @@
+const CACHE_NAME = 'eduhub-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/assets/mascotte.png'
+];
+
+// Installation
 self.addEventListener('install', event => {
   console.log('Service Worker installé');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
 });
 
+// Activation
 self.addEventListener('activate', event => {
   console.log('Service Worker activé');
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
+// Interception des requêtes
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
